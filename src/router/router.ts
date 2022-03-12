03928fb4f87a74ca9ac607b63b19074ff047d6c8
Branislav Stojanović <branislav.stojanovic@blinking.id>
2022-03-12 16:39:48 +0100

    deleted all

--- src/router/router.ts
+++
@@ -1,2 +0,0 @@
-//Purpose of this file is to export all routers from one place
-export {UserRouter} from './user.router'
--- src/utils/validation/validation-schemes.ts
+++
@@ -1,8 +0,0 @@
-import Joi from 'joi';
-
-//Schemes is JS Object which keys are routes
-//and values are JOI schema objects
-
-export const Schemes = {
-    
-}

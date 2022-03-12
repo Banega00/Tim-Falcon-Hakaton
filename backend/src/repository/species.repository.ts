4b49c61df5e44ce4d416a65232fa44f34a5a45f3
Uros Stanimirovic <uros.stanimirovic@blinking.id>
2022-03-12 18:14:43 +0100

    speciesRepo

---
+++ backend/src/repository/species.repository.ts
@@ -0,0 +1,42 @@
+import { EntityManager, getManager } from "typeorm";
+import { Species } from "../entities/Species.entity";
+
+export class _SpeciesRepository{
+
+    public async getAll(entityManager?: EntityManager) {
+        const manager = entityManager || getManager();
+        return manager.find(Species)
+    }
+    
+    public async deleteById(id: string, entityManager?: EntityManager) {
+        const manager = entityManager || getManager();
+        return manager.delete(Species, id)
+    }
+
+    public async findByIdOrCreate(speciesData: Species, entityManager?: EntityManager) {
+        if(!speciesData.id) return ;
+        const species = await this.findById(speciesData.id)
+        if(species) return species;
+
+
+        await this.addNewSpecies(new Species(speciesData));
+    }
+
+    public async findById(speciesId: string, entityManager?: EntityManager): Promise<Species|undefined>{
+        const manager = entityManager || getManager();
+        return await manager.findOne(Species, speciesId)
+    }
+
+    // public async findByEmail(email: string, entityManager?: EntityManager): Promise<Species|undefined>{
+    //     const manager = entityManager || getManager();
+    //     return await manager.findOne(User, {email: email})
+    // }
+
+    public async addNewSpecies(newSpecies: Species, entityManager?: EntityManager) {
+        const manager = entityManager || getManager();
+        return await manager.save(Species, new Species(newSpecies))
+    }
+
+}
+
+export const SpeciesRepository = new _SpeciesRepository();

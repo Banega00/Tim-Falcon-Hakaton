b0279366a3c1cb00146d7396da994aeb123ec473
SaloJovanovic <aleksandarjovanovic670@gmail.com>
2022-03-13 04:12:17 +0100

    login style finished

--- frontend2/src/components/Login/Login.tsx
+++ frontend2/src/components/Login/Login.tsx
@@ -3,6 +3,7 @@ import { useLocation, useNavigate } from "react-router-dom";
 import { useAuth } from "../../utils/Auth";
 import { ResponseModel } from "../../models/ResponseModel";
 import styles from './Login.module.scss'
+import styles1 from '../SpeciesPage/SpeciesPage.module.scss';
 
 export const Login = () => {
   const emailInput = useRef<HTMLInputElement>(null);
@@ -38,18 +39,21 @@ export const Login = () => {
   }
 
   return (
-    <div className={styles.main}>
-      <label htmlFor="email">Email:
-        <input ref={emailInput} type="text" name="email" />
-      </label>
-      <label htmlFor="password">Password:
-        <input ref={passwordInput} type="password" name="password" />
-      </label>
-      <button onClick={handleLogin}>Log in</button>
-      <button onClick={handleGoogleLogin}>Sing in with google</button>
+    <div className={styles1.container}>
+      <div className={styles1.main + " " + styles.main}>
+        <h1>LOGIN</h1>
+        <label htmlFor="email">
+          <input ref={emailInput} placeholder={"Email"} type="text" name="email" />
+        </label>
+        <label htmlFor="password">
+          <input ref={passwordInput} placeholder={"Password"} type="password" name="password" />
+        </label>
+        <button className={styles.loginBtn} onClick={handleLogin}>Log in</button>
+        <button className={styles.signinBtn} onClick={handleGoogleLogin}>Sing in with google</button>
 
-      {/* if server response exists log it*/}
-      {serverResponse && <div>{serverResponse.payload.message}</div>}
+        {/* if server response exists log it*/}
+        {serverResponse && <div>{serverResponse.payload.message}</div>}
+      </div>
     </div>
   );
 };
